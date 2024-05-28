10ef36aad0baa2a97c44363d50b517aee1d35caa
SURAJ ADHIKARI <0089adhikari@gmail.com>
2024-05-28 23:20:02 +0530

    commit

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,10 +2,12 @@
 
 import { useState } from "react";
 import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
 
 const MeetingTypeList = () => {
+  const router = useRouter();
   const [meetingState, setMeetingState] = useState<
-    "isScheduleMeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
   return (
@@ -13,34 +15,30 @@ const MeetingTypeList = () => {
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
-        description="Start new meet"
-        handleClick={() => {
-          setMeetingState("isJoiningMeeting");
-        }}
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        className={"bg-orange-1"}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
       />
       <HomeCard
         img="/icons/schedule.svg"
         title="Schedule Meeting"
-        description="Plan new meet"
-        handleClick={() => {
-          setMeetingState("isScheduleMeeting");
-        }}
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
       />
       <HomeCard
-        img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start new meet"
-        handleClick={() => {
-          setMeetingState("isJoiningMeeting");
-        }}
-      />
-      <HomeCard
-        img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start new meet"
-        handleClick={() => {
-          setMeetingState("isJoiningMeeting");
-        }}
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-yellow-1"
+        handleClick={() => router.push("/recordings")}
       />
     </section>
   );
